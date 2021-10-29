b3b8e113b48f30e988040d3cc58c9c8d93be5298
vim <dan.shumayev@mail.huji.ac.il>
2021-10-29 17:02:00 +0300

    Part 1 is implemented - Test it.

--- EX1/Assets/CharacterAnimator.cs
+++ EX1/Assets/CharacterAnimator.cs
@@ -11,11 +11,18 @@ public class CharacterAnimator : MonoBehaviour
     private BVHData data; // BVH data of the BVHFile will be loaded here
     private int currFrame = 0; // Current frame of the animation
 
+    private static readonly Vector3 SCALE = new Vector3(2.0f, 2.0f, 2.0f);
+    private static readonly Vector3 HEAD_SCALE = new Vector3(8.0f, 8.0f, 8.0f);
+
+
     // Start is called before the first frame update
     void Start()
     {
         BVHParser parser = new BVHParser();
         data = parser.Parse(BVHFile);
+
+        CreateJoint(data.rootJoint, Vector3.zero); // Position the center of our skeleton (Root joint object)
+                                                   // at the origin (World-Space)
     }
 
     // Returns a Matrix4x4 representing a rotation aligning the up direction of an object with the given v
@@ -35,8 +42,40 @@ public class CharacterAnimator : MonoBehaviour
     // Creates a GameObject representing a given BVHJoint and recursively creates GameObjects for it's child joints
     GameObject CreateJoint(BVHJoint joint, Vector3 parentPosition)
     {
-        // Your code here
-        return null;
+        if (joint.isEndSite)
+        {
+            return null;
+        }
+
+        joint.gameObject = new GameObject(joint.name);
+        // Create a sphere representing the joint, and make it a child of joint
+        GameObject jointSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        jointSphere.transform.parent = joint.gameObject.transform;
+
+        // Apply Scale Transform' on the sphere
+        ScaleSphere(joint, jointSphere);
+
+        // Apply Translate Transform' on joint
+        MatrixUtils.ApplyTransform(joint.gameObject, MatrixUtils.Translate(joint.offset + parentPosition));
+
+        foreach (BVHJoint child in joint.children)
+        {
+            CreateJoint(child, joint.offset + parentPosition);
+        }
+
+        return joint.gameObject;
+    }
+
+    private void ScaleSphere(BVHJoint joint, GameObject jointSphere)
+    {
+        if (joint.name.Equals("HEAD"))
+        {
+            MatrixUtils.ApplyTransform(jointSphere, MatrixUtils.Scale(SCALE));
+        }
+        else
+        {
+            MatrixUtils.ApplyTransform(jointSphere, MatrixUtils.Scale(HEAD_SCALE));
+        }
     }
 
     // Transforms BVHJoint according to the keyframe channel data, and recursively transforms its children
