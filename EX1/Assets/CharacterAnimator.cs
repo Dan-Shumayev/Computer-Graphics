1ff079e2f7e4db823f6f85384b438639054c7f3e
vim <dan.shumayev@mail.huji.ac.il>
2021-10-30 21:10:23 +0300

    Part 2 - RotateTowardsVector - Not sure about its validity - to be reviewed.

--- EX1/Assets/CharacterAnimator.cs
+++ EX1/Assets/CharacterAnimator.cs
@@ -22,13 +22,36 @@ public class CharacterAnimator : MonoBehaviour
 
         CreateJoint(data.rootJoint, Vector3.zero); // Position the center of our skeleton (Root joint object)
                                                    // at the origin (World-Space)
+
+        Debug.Log(RotateTowardsVector(new Vector3(1.0f, 1.0f, 1.0f)).MultiplyVector(Vector3.up));
+        Debug.Log(MatrixUtils.RotateTowardsVector(new Vector3(1.0f, 1.0f, 1.0f)).MultiplyVector(Vector3.up));
     }
 
-    // Returns a Matrix4x4 representing a rotation aligning the up direction of an object with the given v
-    Matrix4x4 RotateTowardsVector(Vector3 v)
+    // Returns a Matrix4x4 representing a rotation aligning 
+    // the up direction of an object with the given v
+    Matrix4x4 RotateTowardsVector(Vector3 v) // TODO - to be tested/debugged!
     {
-        // Your code here
-        return Matrix4x4.zero;
+        // The normal of the plane spanned by `v` and the object to be directed
+        Vector3 rotationAxis = Vector3.Cross(v.normalized, transform.up);
+        // v X transform.up = |v.normalized|*|transform.up|*Sin(alpha) (Cross-product)
+        float rotationAngle = Mathf.Asin(rotationAxis.magnitude) * Mathf.Rad2Deg;
+
+        // The rotation process described in TA-2
+        return RotateAroundVec(rotationAxis.normalized, rotationAngle);
+    }
+
+    // Summary:
+    //      This method returns a 4X4 transform' matrix that rotates our object by 
+    //      alphaAngle degrees around the received vector, `v`.
+    Matrix4x4 RotateAroundVec(Vector3 v, float rotationAngle)
+    {
+        // Find the angles that `v` induces on each of the 3-axes - and return rotation transorm' accordingly
+        Matrix4x4 xRotation = MatrixUtils.RotateX(90.0f - Mathf.Atan2(v.z, v.y) * Mathf.Rad2Deg);
+        Matrix4x4 zRotation = MatrixUtils.RotateZ(90.0f -
+                        Mathf.Atan2(Mathf.Sqrt(Mathf.Pow(v.y, 2) + Mathf.Pow(v.z, 2)), v.x) * Mathf.Rad2Deg);
+        Matrix4x4 yRotation = MatrixUtils.RotateY(rotationAngle);
+
+        return xRotation.inverse * zRotation.inverse * yRotation * zRotation * xRotation;
     }
 
     // Creates a Cylinder GameObject between two given points in 3D space
@@ -38,7 +61,7 @@ public class CharacterAnimator : MonoBehaviour
         return null;
     }
 
-    // Creates a GameObject representing a given BVHJoint and recursively creates GameObjects for it's child joints
+    // Creates a GameObject representing a given BVHJoint and recursively creates GameObjects for its child joints
     GameObject CreateJoint(BVHJoint joint, Vector3 parentPosition)
     {
         if (joint.isEndSite)
